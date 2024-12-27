9d6f7a53b5a3e77e682c011ff3ae2633737dfec3
Arjun Mishra <154399579+arjunmishraaa@users.noreply.github.com>
2024-12-27 23:52:45 +0530

    Add files via upload

--- Kalistee/src/pages/Home/Home.js
+++ Kalistee/src/pages/Home/Home.js
@@ -6,7 +6,11 @@ import NewArrivals from "../../components/home/NewArrivals/NewArrivals";
 import Sale from "../../components/home/Sale/Sale";
 import SpecialOffers from "../../components/home/SpecialOffers/SpecialOffers";
 import YearProduct from "../../components/home/YearProduct/YearProduct";
-
+// import Marquee from "../../components/home/Marque/Marque";
+import Marque2 from "../../components/home/Marque/Marque2";
+import GsapAnim from "../../components/gsapAnim/GsapAnim";
+import a1 from '../../components/gsapAnim/imgAnim/a1.png';
+import a2 from '../../components/gsapAnim/imgAnim/a2.png';
 const Home = () => {
   return (
     <div className="w-full mx-auto">
@@ -18,6 +22,9 @@ const Home = () => {
         <BestSellers />
         <YearProduct />
         <SpecialOffers />
+        <Marque2 />
+        <GsapAnim />
+
       </div>
     </div>
   );
