ee949f0e241fded6652799d2cc1ded3ae5a80659
arynch24 <arynch24@gmail.com>
2024-12-28 11:13:59 +0530

    desc fixed

--- Kalistee/src/components/home/SpecialOffers/SpecialOffers.js
+++ Kalistee/src/components/home/SpecialOffers/SpecialOffers.js
@@ -9,19 +9,17 @@ const SpecialOffers = () => {
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
         <Product
           _id="1101"
-          img={'https://ik.imagekit.io/Jivan/BrightningSerum.png?updatedAt=1735335883052'}
+          img={'https://ik.imagekit.io/Jivan/BrightningSerum.png?updatedAt=1735339160654'}
           productName="Brightening Serum"
           price="599.00"
-          // color="Blank and White"
           badge={true}
           des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
         />
         <Product
           _id="1102"
-          img={'https://ik.imagekit.io/durgeshsh/NodeX_images/rejuvenating_hair_oil_bhringraj_outdoor.png?updatedAt=1735332166746'}
+          img={'https://ik.imagekit.io/durgeshsh/NodeX_images/rejuvenating_hair_oil_bhringraj_outdoor.png?updatedAt=1735338729008'}
           productName="Rejuvenating Hair Oil"
           price="249.00"
-          // color="Gray"
           badge={true}
           des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
         />
@@ -30,7 +28,6 @@ const SpecialOffers = () => {
           img={'https://ik.imagekit.io/Jivan/HairMask.png?updatedAt=1735336513805'}
           productName="Anti-Frizz Hair Mask"
           price="399.00"
-          color="Mixed"
           badge={true}
           des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
         />
@@ -39,7 +36,6 @@ const SpecialOffers = () => {
           img={'https://ik.imagekit.io/Jivan/NightrepairingCream.png?updatedAt=1735335883142'}
           productName="Night Repair Cream"
           price="799.00"
-          color="Black"
           badge={false}
           des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
         />
