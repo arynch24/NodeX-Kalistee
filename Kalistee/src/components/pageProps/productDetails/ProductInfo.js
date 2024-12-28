8eac0c23f6218510640ec08343a7aef57f932f0e
arynch24 <arynch24@gmail.com>
2024-12-28 11:33:03 +0530

    last last fixes

--- Kalistee/src/components/pageProps/productDetails/ProductInfo.js
+++ Kalistee/src/components/pageProps/productDetails/ProductInfo.js
@@ -29,8 +29,7 @@ const ProductInfo = ({ productInfo }) => {
         Add to Cart
       </button>
       <p className="font-normal text-sm">
-        <span className="text-base font-medium"> Categories:</span> Spring
-        collection, Streetwear, Women Tags: featured SKU: N/A
+        <span className="text-base font-medium"> Categories:</span>  Natural Skin Care & Lifestyle Products
       </p>
     </div>
   );
