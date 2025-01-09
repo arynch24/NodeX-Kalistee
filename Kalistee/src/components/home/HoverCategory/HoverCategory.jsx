29811fd76000f1d59b98a8336d2b3cbdbb2b41cd
arynch24 <arynch24@gmail.com>
2025-01-09 22:23:12 +0530

    hover images fixed

--- Kalistee/src/components/home/HoverCategory/HoverCategory.jsx
+++ Kalistee/src/components/home/HoverCategory/HoverCategory.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
 
 const HoverCategory = () => {
-    // const history = useHistory();
-
-    // const handleImageClick = (pageUrl) => {
-    //     history.push(pageUrl);
-    // };
 
     return (
         <div className="flex justify-center align-middle gap-2 my-20" >
             <div className="column  border-black border-[2px] rounded-lg">
                 <img
-                    src="https://ik.imagekit.io/Jivan/4.png?updatedAt=1735336905440"
-                    alt="Image 1"
-                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/3.png?updatedAt=1735336905512')}
-                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/4.png?updatedAt=1735336905440')}
-                    // onClick={() => handleImageClick('/page1')}
+                    src="https://ik.imagekit.io/Jivan/Skin2.svg?updatedAt=1736438300506"
+                    alt="Skin"
+                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Skin.svg?updatedAt=1736438308853')}
+                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Skin2.svg?updatedAt=1736438300506')}
                     className='rounded-lg'
                 />
             </div>
             <div className="column  border-black border-2 rounded-lg">
                 <img
-                    src="https://ik.imagekit.io/Jivan/6.png?updatedAt=1735336905512"
-                    alt="Image 3"
-                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/5.png?updatedAt=1735336905454')}
-                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/6.png?updatedAt=1735336905512')}
-                    // onClick={() => handleImageClick('/page2')}
+                    src="https://ik.imagekit.io/Jivan/Hair2.svg?updatedAt=1736438299635"
+                    alt="Hair"
+                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Hair.svg?updatedAt=1736438309775')}
+                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Hair2.svg?updatedAt=1736438299635')}
                     className='rounded-lg'
                 />
             </div>
             <div className="column  border-black border-2 rounded-lg">
                 <img
-                    src="https://ik.imagekit.io/Jivan/2.png?updatedAt=1735336905515"
-                    alt="Image 5"
-                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/1.png?updatedAt=1735336905516')}
-                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/2.png?updatedAt=1735336905515')}
-                    // onClick={() => handleImageClick('/page3')}
+                    src="https://ik.imagekit.io/Jivan/Gum2.svg?updatedAt=1736438299815"
+                    alt="Gummies"
+                    onMouseOver={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Gum.svg?updatedAt=1736438309450')}
+                    onMouseOut={(e) => (e.currentTarget.src = 'https://ik.imagekit.io/Jivan/Gum2.svg?updatedAt=1736438299815')}
                     className='rounded-lg'
                 />
             </div>
